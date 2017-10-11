44f3a9fd515ab0c4abe038484b4f3bcda5d57fb5
Michal Polovka <michal.polovka@gmail.com>
2017-10-11 17:36:58 +0200

    Formatting changes to comply PEP8.

--- model/gameboard.py
+++ model/gameboard.py
@@ -6,13 +6,16 @@ from random import randint
 
 class Gameboard:
     """
-    Class representing gameboard consisting of guessed pattern, status field and possible colors to choose from.
+    Class representing gameboard consisting of guessed pattern,
+    status field and possible colors to choose from.
     """
     def __init__(self, number_of_colors=8, pattern_size=5, attempts=12):
         self.number_of_colors = number_of_colors
         self.pattern_size = pattern_size
         self.attempts = attempts
-        self.colors = ["RED", "GREEN", "BLUE", "YELLOW", "WHITE", "BLACK", "CYAN"][:number_of_colors+1]
+        self.colors = [
+                "RED", "GREEN", "BLUE", "YELLOW", "WHITE", "BLACK", "CYAN"
+                ][:number_of_colors+1]
         self.pattern = []
         self.guessed = []
         self.evaluation = []
@@ -21,7 +24,9 @@ class Gameboard:
         self.pattern = "44444"
         # chosen by fair dice roll
         # guaranteed to be random
-        self.pattern = "".join([str(randint(0, self.number_of_colors)) for _ in range(0, self.pattern_size)])
+        self.pattern = "".join(
+                [str(randint(0, self.number_of_colors)) 
+                    for _ in range(0, self.pattern_size)])
 
     def guess_pattern(self):
         pass
